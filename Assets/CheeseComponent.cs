f2483618102634c0035f9a4e8eb1b4439a67d58f
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-04-21 16:33:49 -0400

    added sway for collectibles

--- Assets/CheeseComponent.cs
+++ Assets/CheeseComponent.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class CheeseComponent : MonoBehaviour
 {
+    [SerializeField] float amplitude;
+    [SerializeField] float frequency;
+    float elapsedTime = 0;
+    float previousDisplacement = 0;
+
+    private void Awake()
+    {
+        StartCoroutine(MoveOverTime());
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GetComponentInChildren<SpriteRenderer>().enabled = false;
-        GetComponent<CircleCollider2D>().enabled = false;     
+        GetComponent<CircleCollider2D>().enabled = false;    
+        StopCoroutine(MoveOverTime());
     }
-
+    IEnumerator MoveOverTime()
+    {
+        while (true)
+        {
+            float displacement = amplitude * Mathf.Sin(frequency * 2 * Mathf.PI * (elapsedTime += Time.deltaTime));
+            transform.Translate((displacement - previousDisplacement) * Vector2.up);
+            previousDisplacement = displacement;
+            yield return null;
+        }
+    }
 }
