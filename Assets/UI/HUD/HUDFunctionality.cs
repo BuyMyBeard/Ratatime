df13de01ecb39b7919de2b5be73955a0a214f4b4
SethJustus <sethbarrere@gmail.com>
2023-04-23 22:25:17 -0400

    Pause

--- Assets/UI/HUD/HUDFunctionality.cs
+++ Assets/UI/HUD/HUDFunctionality.cs
@@ -2,21 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class HUDFunctionality : MonoBehaviour
 {
-    private VisualElement root;
+    [SerializeField] VisualTreeAsset HUD;
+    [SerializeField] VisualTreeAsset PauseMenu;
+
+    private UIDocument doc;
     private Label countdownTimer;
+    private Button pauseButton;
+    //private Button playButton;
     private Game_Manager manager;
 
     void Start()
     {
-        root = GetComponent<UIDocument>().rootVisualElement;
-        countdownTimer = root.Q<Label>("Timer");
-        manager = GameObject.FindObjectOfType<Game_Manager>();
-        SetTimer(null, null);
-        manager.TimeChanged += SetTimer;
+        doc = GetComponent<UIDocument>();
+        Play();
     }
 
     string GetDisplayTime(int time)
@@ -37,5 +40,40 @@ public class HUDFunctionality : MonoBehaviour
     void SetTimer(object sender, EventArgs e)
     {
         countdownTimer.text = GetDisplayTime(manager.Time);
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+        doc.visualTreeAsset = PauseMenu;
+        var playButton = doc.rootVisualElement.Q<Button>("PlayButton");
+        playButton.clicked += Play;
+
+        var restartButton = doc.rootVisualElement.Q<Button>("RestartButton");
+        restartButton.clicked += Restart;
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Play()
+    {
+        Time.timeScale = 1;
+        doc.visualTreeAsset = HUD;
+
+        countdownTimer = doc.rootVisualElement.Q<Label>("Timer");
+        pauseButton = doc.rootVisualElement.Q<Button>("PauseButton");
+        manager = FindObjectOfType<Game_Manager>();
+
+        SetTimer(null, null);
+        manager.TimeChanged += SetTimer;
+        pauseButton.clicked += Pause;
+
+        doc.rootVisualElement.RegisterCallback<NavigationSubmitEvent>((evt) =>
+        {
+            evt.StopPropagation();
+        }, TrickleDown.TrickleDown);
     }
 }
